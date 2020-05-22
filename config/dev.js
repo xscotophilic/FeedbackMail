527262bd5801e8a90ff20ad4edc2bede761a137c
Chintan <chintan.170410116089@gmail.com>
2020-05-22 18:06:11 +0530

    Delete dev.js

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    googleClientID: '129049501679-dhbupsbbhr2g780kndm9ok916sef44uu.apps.googleusercontent.com',
-    googleClientSecret: '1Cm4aknKcNwPUiY3dBEFOBP7',
-    mongoURI: 'mongodb+srv://tyrion404:Noone404_5593@feedbackmail-khswg.mongodb.net/fedatabase?retryWrites=true&w=majority',
-    cookieKey: 'ad66394b5df38be39ad0c9fdff58f533'
-};
