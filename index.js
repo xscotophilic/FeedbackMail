e771d69f1f3bccd78c3950559cce8d9c9e6352ed
tyrion404 <chintan.170410116089@gmail.com>
2020-05-25 16:15:04 +0530

    sendgrid backend in progress

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 
 const keys = require('./config/keys');
 require('./models/user');
+require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI, { useUnifiedTopology: true, useNewUrlParser: true });
@@ -27,6 +28,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
