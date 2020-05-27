02a123198ab7b38094db7634bcb41e3f806ecac0
tyrion404 <chintan.170410116089@gmail.com>
2020-05-27 17:42:57 +0530

    create survey and sendgrid backend stuff

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
 
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+
+    app.get('/api/surveys/thanks', (req, res) => {
+        res.send("Thanks for voting.");
+        res.end();
+    });
+
+    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const { title, subject, body, recipients } = req.body;
         const survey = new Survey({
             title,
@@ -16,5 +24,16 @@ module.exports = app => {
             _user: req.user.id,
             dateSent: Date.now()
         });
+        const mailer = new Mailer(survey, surveyTemplate(survey));
+        try {
+            await mailer.send();
+            await survey.save();
+            req.user.credits -= 1;
+            const user = await req.user.save();
+
+            res.send(user);
+        } catch (err) {
+            res.status(422).send(err);
+        }
     });
 };
