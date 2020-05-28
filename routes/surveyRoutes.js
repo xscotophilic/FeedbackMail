84ff705e1ecbad19bc95df526b52416002b8ec90
tyrion404 <chintan.170410116089@gmail.com>
2020-05-28 18:12:59 +0530

    Frontend of creating survey is done.

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const { Path } = require('path-parser');
+const _ = require('lodash');
+const { URL } = require('url');
 
 const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
@@ -9,8 +12,37 @@ const Survey = mongoose.model('surveys');
 
 module.exports = app => {
 
-    app.get('/api/surveys/thanks', (req, res) => {
-        res.send("Thanks for voting.");
+    app.get('/api/surveys/:id/:choice', (req, res) => {
+        res.send("Thanks for your response.");
+        res.end();
+    });
+
+    app.post('/api/surveys/webhooks', (req, res) => {
+        const p = new Path('/api/surveys/:surveyId/:choice');
+        _.chain(req.body)
+            .map(({ email, url }) => {
+                const match = p.test(new URL(url).pathname);
+                if (match) {
+                    return { email, surveyId: match.surveyId, choice: match.choice };
+                }
+            })
+            .compact()
+            .uniqBy('email', 'surveyId')
+            .each(({ email, surveyId, choice }) => {
+                Survey.updateOne({
+                    _id: surveyId,
+                    recipients: {
+                        $elemMatch: { email: email, responded: false }
+                    }
+                }, {
+                    $inc: { [choice]: 1 },
+                    $set: { 'recipients.$.responded': true },
+                    lastResponded: new Date()
+                }).exec();
+            })
+            .value();
+
+        res.send({});
         res.end();
     });
 
